4aba4e5221d55dd35a0508044f3f3af12dbaabbe
fadhilansyah25 <fadhilansyah25@gmail.com>
2021-03-05 22:19:41 +0700

    Create view Category/Bank/Item/Booking Dashboard admin

--- controller/admin_controller.js
+++ controller/admin_controller.js
@@ -1,5 +1,17 @@
 module.exports = {
-    viewDashboard: (req, res) => {
-        res.render('admin/dashboard/view_dashboard.ejs');
-    }
-}
+  viewDashboard: (req, res) => {
+    res.render("admin/dashboard/view_dashboard.ejs");
+  },
+  viewCategory: (req, res) => {
+    res.render("admin/category/view_category.ejs");
+  },
+  viewBank: (req, res) => {
+    res.render("admin/bank/view_bank.ejs");
+  },
+  viewitem: (req, res) => {
+    res.render("admin/item/view_item.ejs");
+  },
+  viewBooking: (req, res) => {
+    res.render("admin/booking/view_booking.ejs");
+  },
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,11 @@
-const router = require('express').Router();
-const adminController = require('../controller/admin_controller');
+const router = require("express").Router();
+const { route } = require(".");
+const adminController = require("../controller/admin_controller");
 
-router.get('/dashboard', adminController.viewDashboard);
+router.get("/dashboard", adminController.viewDashboard);
+router.get("/category", adminController.viewCategory);
+router.get("/bank", adminController.viewBank);
+router.get("/item", adminController.viewitem);
+router.get("/booking", adminController.viewBooking);
 
 module.exports = router;
