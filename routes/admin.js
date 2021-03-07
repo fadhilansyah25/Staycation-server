4cfb899ced3e06b3f61233ffad3a983352a5de86
fadhilansyah25 <fadhilansyah25@gmail.com>
2021-03-07 21:21:23 +0700

    Connect to Database | CRUD Category | Create Alert & Title

--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,16 @@ const { route } = require(".");
 const adminController = require("../controller/admin_controller");
 
 router.get("/dashboard", adminController.viewDashboard);
+
 router.get("/category", adminController.viewCategory);
+router.post("/category", adminController.addCategory);
+router.put("/category", adminController.editCategory);
+router.delete("/category/:id", adminController.deleteCategory);
+
 router.get("/bank", adminController.viewBank);
+
 router.get("/item", adminController.viewitem);
+
 router.get("/booking", adminController.viewBooking);
 
 module.exports = router;
